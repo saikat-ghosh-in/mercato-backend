0c593db2d33cbd0ccbfedad6c8c7714b106d0e54
SaikatG777 <007saikatghosh@gmail.com>
2025-12-29 18:53:28 +0530

    implemented basic auth with a sample userId and password

---
+++ src/main/java/com/ecommerce_backend/Configuration/SpringSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ecommerce_backend.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
